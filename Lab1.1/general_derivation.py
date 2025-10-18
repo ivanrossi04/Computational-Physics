2514d9a9556063b4388206ea62021ef82800fb87
ivanrossi04 <ivanrossi04@gmail.com>
2025-10-18 17:45:53 +0200

    Improved general derivation and added comments
    General derivation now supports arbitrary functions, which can be declared in the program before execution, and odd-order derivatives, although with the disadvantage of calculating a higher-order derivative.
    
    Fixed the types of the derivative_function and added the needed exceptions.

--- Lab1.1/general_derivation.py
+++ Lab1.1/general_derivation.py
@@ -1,59 +1,70 @@
 # !V1
 
+'''
+How to run this script:
+- set function in the main() function
+- run the script
+- input the interval, number of points and derivative order when prompted
+'''
+
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 
-# TODO: the program only works for sin(x), make it general
-# TODO: the program only works for even n derivatives, make it general
-
 def main():
+
+    x_min = float(input('Input the minimum value of x:'))
     x_max = float(input('Input the maximum value of x:'))
-    n=int(input('Input the number of points (precision of the approximation):'))
+    n = int(input('Input the number of points (precision of the approximation):'))
     deriv_n = int(input('Input the order of the derivative: '))
 
     # compute the function values at the determined intervals
-    x=np.linspace(0,x_max,n)
-    f=np.sin(x)
+    x = np.linspace(x_min,x_max,n)
+    
+    # ! define manually the function
+    f = np.sin
+    # f = lambda x: np.exp(-x**2)
+    # ! ----------------------------
+
+    y = f(x)
 
     # compute the interval delta
-    h = x_max / (n - 1)
+    h = (x_max - x_min) / (n - 1)
 
     # init the derivative function value list
     df = np.zeros(n, dtype=np.float64)
 
+    # dimension of the A matrix
+    # an additional row and column is computed if deriv_n is odd
+    a_m_dim = deriv_n + 1 + (deriv_n % 2)
+
+    # compute the offsets and factorials needed for the Taylor expansion (for efficiency)
+    steps = [(deriv_n/2 - k) * h for k in range(a_m_dim)] # starts from the rightmost point and goes to the left
+    factorials = [np.math.factorial(k) for k in range(a_m_dim)]
+
     for i in range(n): # for every x_i values
-        f_i = np.zeros(deriv_n + 1)
+        f_i = np.zeros(a_m_dim)
         
         # populate the a_matrix with the needed values from the Taylor approximation
-        a_m = np.ones((deriv_n + 1,deriv_n + 1),dtype=np.float64)
-        for j in range(deriv_n + 1):            
+        a_m = np.ones((a_m_dim, a_m_dim), dtype=np.float64)
+        for j in range(a_m_dim):
             # compute the f_i needed for the derivative approximation
-            f_i[j] = np.sin(x[i] + (deriv_n/2 - j) * h)
+            f_i[j] = f(x[i] + steps[j])
 
-            for k in range(1, deriv_n + 1):
-                a_m[j][k] = ((deriv_n/2 - j) * h) ** k / math.factorial(k)
+            for k in range(1, a_m_dim):
+                a_m[j][k] = (steps[j] ** k) / factorials[k]
         
-        # print('F_', x[i] , ': ', f_i, '\n')
-        # print(a_m)
-        
-        # invert the a matrix and solve the linear system to find the n-th derivative
-        a_m_inv = np.linalg.inv(a_m)
-        df[i] = np.matmul(a_m_inv, f_i)[deriv_n] # only storing the n-th derivative
+        # solve the linear system to find the n-th derivative
+        df[i] = np.linalg.solve(a_m, f_i)[deriv_n]
 
     # plotting the function
-    fig, ax = plt.subplots()
-
     plt.rcParams.update({'font.size': 14})
-    ax.set_xlim(0,x_max)
-    ax.set_ylim(-1,1)
+    plt.figure()
+    plt.plot(x, f(x),  ls='-', label="F(x)")
+    plt.plot(x, df,  ls='-', label= str(deriv_n) + " derivative approx.")
 
-    plt.plot(x,f,  ls='-', label="Sin")
-    plt.plot(x,df,  ls='-', label= str(deriv_n) + " derivative approx.")
-
-    ax.legend()
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
+    plt.legend()
+    plt.xlabel("X")
+    plt.ylabel("Y")
     plt.show(block=True)
 
 if __name__ == "__main__":
