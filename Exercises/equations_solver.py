6b7a265250430816f05725a602d0fbff26b96607
ivanrossi04 <ivanrossi04@gmail.com>
2025-10-23 18:31:34 +0200

    Added different methods to compute the zeros of an equation
    Updated equations_solver by moving the algorithm to a function and adding its recursive version alongside a different method.
    
    The Newton-Rhapson method was added with a recursive version and a constant space interval h for derivation.

--- Exercises/equations_solver.py
+++ Exercises/equations_solver.py
@@ -9,6 +9,7 @@ Choose the interval [a, b] appropriately.
 '''
 
 import math
+from typing import Callable
 
 # Auxiliary function to determine the sign of a number
 def sign(x: float) -> int:
@@ -24,16 +25,8 @@ def sign(x: float) -> int:
 
     return (x > 0) - (x < 0)
 
-def main():
-
-    # ! Define the function to solve and the initial parameters
-    f = lambda x: math.sin(x)
-    a = -1
-    b = 1
-    epsilon = 1e-10
-    # ! -------------------------------------------------------
-
-    answer = None
+def solve_bisection(f: Callable[[float], float], a: float, b: float, epsilon: float) -> float:
+    answer = math.nan
     f_a = f(a)
     f_b = f(b)
 
@@ -41,23 +34,74 @@ def main():
     if(sign(f_a) * sign(f_b) < 0):
         while b - a > epsilon:
             c = (a + b) / 2
-            answer = f(c)
-
-            # Debug statement
-            # print("Current interval: [", a, ", ", c, ", ", b, "] -> f(c) = ", answer)
-
-            if sign(f_a) * sign(answer) < 0:
+            answer = c
+            f_c = f(c)
+            if sign(f_a) * sign(f_c) < 0:
                 b = c
-                f_b = answer
-            elif sign(answer) * sign(f_b) < 0:
+                f_b = f_c
+            elif sign(f_c) * sign(f_b) < 0:
                 a = c
-                f_a = answer
+                f_a = f_c
             else:
                 break # answer is zero in this case
-    elif f(b) == 0: answer = f(b)
-    elif f(a) == 0: answer = f(a)
+    elif f(b) == 0: answer = b
+    elif f(a) == 0: answer = a
 
-    print("The root is approximately at: ", answer)
+    return answer
+
+def solve_bisection_recursive(f: Callable[[float], float], a: float, b: float, epsilon: float) -> float:
+    answer = math.nan
+
+    if b - a > epsilon:
+        f_a = f(a)
+        f_b = f(b)
+
+        # Bisection method (recursive)
+        if(sign(f_a) * sign(f_b) < 0):
+            c = (a + b) / 2
+            f_c = f(c)
+
+            if sign(f_a) * sign(f_c) < 0: answer = solve_bisection_recursive(f, a, c, epsilon)
+            elif sign(answer) * sign(f_b) < 0: answer = solve_bisection_recursive(f, c, b, epsilon)
+            else: answer = c
+        elif f_b == 0: answer = a
+        elif f_a == 0: answer = b
+    else: answer = b
+    
+    return answer
+
+h = 1e-5
+
+def solve_newton_rhapson(f: Callable[[float], float], x_trial: float, epsilon: float) -> float:
+    x_sol = 0.0
+
+    while True:
+        df_dx = (f(x_trial + h) - f(x_trial - h)) / (2 * h) 
+        x_sol = x_trial - f(x_trial)/df_dx
+
+        if(abs(x_trial - x_sol) < epsilon): break
+        
+        x_trial = x_sol 
+
+    return x_sol
+
+def solve_newton_rhapson_recursive(f: Callable[[float], float], x_trial: float, epsilon: float) -> float:
+    df_dx = (f(x_trial + h) - f(x_trial - h)) / (2 * h) 
+    x_sol = x_trial - f(x_trial) / df_dx
+
+    if(abs(x_trial - x_sol) > epsilon): return solve_newton_rhapson_recursive(f, x_sol, epsilon)
+    return x_sol
+
+def main():
+
+    # ! Define the function to solve and the initial parameters
+    f = lambda x: math.sin(x-1)
+    a = 0
+    b = 2
+    epsilon = 1e-15
+    # ! -------------------------------------------------------
+
+    print("The root is approximately at: ", solve_newton_rhapson(f, a, epsilon))
 
 if __name__ == "__main__":
     main()
